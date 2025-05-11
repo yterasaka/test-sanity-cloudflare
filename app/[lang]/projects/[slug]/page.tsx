68b17d951596337eca8b01427213e1c01f817ea0
Yuki Terasaka <yuki.terasaka@gmail.com>
2025-05-11 14:05:35 +0900

    Update 2

--- app/[lang]/projects/[slug]/page.tsx
+++ app/[lang]/projects/[slug]/page.tsx
@@ -13,8 +13,6 @@ import {draftMode} from 'next/headers'
 import Link from 'next/link'
 import {notFound} from 'next/navigation'
 
-export const runtime = 'edge'
-
 type Props = {
   params: Promise<{lang: string; slug: string}>
 }
